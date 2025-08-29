9385b7fdee44c12ad0aa0ac7a3bc094b58fe93c8
Diego Chavarro <diego.chavarro-c@mail.escuelaing.edu.co>
2025-08-29 01:51:51 -0500

    creacion de proyecto maven

---
+++ src/test/java/org/example/SanityTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SanityTest {
+    @Test
+    void shouldPass() {
+        assertTrue(2 + 2 == 4);
+    }
+}
